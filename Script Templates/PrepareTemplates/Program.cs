6d7e9fcf4a0ad9758271160cfcfdd975b670a2f2
Andrew Forget <andrew.forget@andydragon.com>
2024-01-05 20:46:16 -0500

    Adding the hubs catalog file

--- Script Templates/PrepareTemplates/Program.cs
+++ Script Templates/PrepareTemplates/Program.cs
@@ -5,6 +5,7 @@ class Program
     static void Main()
     {
         var cwd = Directory.GetCurrentDirectory();
+        var templateCatalog = new TemplateCatalog();
         foreach (var folder in Directory.EnumerateDirectories(cwd).Order())
         {
             var templates = new Dictionary<string, string>();
@@ -17,7 +18,10 @@ class Program
             }
             if (templates.Count != 0)
             {
-                using var stringDict = File.CreateText(Path.Combine(cwd, folder[(cwd.Length + 1)..] + ".stringsdict"));
+                var hubName = folder[(cwd.Length + 1)..];
+                templateCatalog.Hubs.Add(new Hub(hubName, templates.Keys.ToList()));
+
+                using var stringDict = File.CreateText(Path.Combine(cwd, hubName + ".stringsdict"));
                 stringDict.WriteLine("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
                 stringDict.WriteLine("<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">");
                 stringDict.WriteLine("<plist version=\"1.0\">");
@@ -30,13 +34,42 @@ class Program
                 stringDict.WriteLine("</dict>");
                 stringDict.WriteLine("</plist>");
 
-                using var xcStrings = File.CreateText(Path.Combine(cwd, "new_" + folder[(cwd.Length + 1)..] + ".xcstrings"));
+                using var xcStrings = File.CreateText(Path.Combine(cwd, "new_" + hubName + ".xcstrings"));
                 var strings = new StringsCatalog(templates);
                 var result = Newtonsoft.Json.JsonConvert.SerializeObject(strings, Newtonsoft.Json.Formatting.Indented).Replace("\": \"", "\" : \"").Replace("\": {", "\" : {");
                 xcStrings.WriteLine(result);
             }
         }
+        using var catalogFile = File.CreateText(Path.Combine(cwd, "hubs.json"));
+        var catalogJson = Newtonsoft.Json.JsonConvert.SerializeObject(templateCatalog, Newtonsoft.Json.Formatting.Indented);
+        catalogFile.WriteLine(catalogJson);
     }
+}
+
+class TemplateCatalog
+{
+    public TemplateCatalog()
+    {
+        Hubs = new List<Hub>();
+    }
+
+    [Newtonsoft.Json.JsonProperty(propertyName: "hubs")]
+    public IList<Hub> Hubs { get; private set; }
+}
+
+class Hub
+{
+    public Hub(string name, List<string> templates)
+    {
+        Name = name;
+        Templates = templates;
+    }
+
+    [Newtonsoft.Json.JsonProperty(propertyName: "name")]
+    public string Name { get; }
+
+    [Newtonsoft.Json.JsonProperty(propertyName: "templates")]
+    public List<string> Templates { get; }
 }
 
 class StringsCatalog
